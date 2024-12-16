d4cb8eee258885f438dc44e8b69ad48baa6f5bdb
Tharindu Ranaweera <tharindu.ranaweera@sanoma.com>
2024-12-16 23:51:07 +0200

    Fixed some refactoring issues

--- part_a/dual_image_model.py
+++ part_a/dual_image_model.py
@@ -1,21 +1,34 @@
 import copy
 import torch.nn as nn
 import torch
+from torchvision import models
 
+pre_trained_models = {
+        "resnet18": models.resnet18(pretrained=True),
+        "resnet34": models.resnet34(pretrained=True),
+        "vgg": models.vgg16(pretrained=True),
+        "efficientnet_b0": models.efficientnet_b0(pretrained=True),
+        "densenet121": models.densenet121(pretrained=True),
+}
 
 class DualImageModel(nn.Module):
     def __init__(self, model, num_classes=5, dropout_rate=0.5):
         super().__init__()
 
-        backbone = model
+        backbone = pre_trained_models[model]
         backbone.fc = nn.Identity()
 
         # Here the two backbones will have the same structure but unshared weights
         self.backbone1 = copy.deepcopy(backbone)
         self.backbone2 = copy.deepcopy(backbone)
 
+        if model not in ["resnet18", "resnet34"]:
+            input_features = 1000
+        else:
+            input_features = 512
+
         self.fc = nn.Sequential(
-            nn.Linear(512 * 2, 256),
+            nn.Linear(input_features * 2, 256),
             nn.ReLU(inplace=True),
             nn.Dropout(p=dropout_rate),
             nn.Linear(256, 128),
